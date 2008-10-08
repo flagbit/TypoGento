f26d064a5bbe17b4bead6c7be9b0038a1b5a4610
hackwell <weller@flagbit.de>
2008-10-08 07:39:47 +0000

    Bugfix Database Connection

--- Magento/Typo3connect/Model/Customer.php
+++ Magento/Typo3connect/Model/Customer.php
@@ -1,50 +1,45 @@
 <?php
 
-
-
 class Flagbit_Typo3connect_Model_Customer extends Mage_Customer_Model_Customer {
-
 	
 	/**
 	 * create or update an TYPO3 Frontend User
 	 *
 	 */
-	public function _afterSave(){
+	public function _afterSave() {
 		
-		parent::_afterSave();
+		parent::_afterSave ();
 		
-		if (! Mage::getSingleton ( 'Flagbit_Typo3connect/Core' )->isEnabled ()) return;
+		if (! Mage::getSingleton ( 'Flagbit_Typo3connect/Core' )->isEnabled ())
+			return;
 		
-
-		$fields = array(
-			'username' => $this->getData('email'),
-			'name' => $this->getData('lastname'),
-			'firstname' => $this->getData('firstname'),
-			'email' => $this->getData('email'),
-			'password' => $this->getData('password'),
-			'usergroup' => Mage::helper('Flagbit_Typo3connect')->getConfigData('fe_user_group_uid'),
-			'pid' => Mage::helper('Flagbit_Typo3connect')->getConfigData('fe_user_pid'),
-			'tx_fbmagento_id' => $this->getId(),
+		$fields = array (
+			'username' => $this->getData ( 'email' ), 
+			'name' => $this->getData ( 'lastname' ), 
+			'firstname' => $this->getData ( 'firstname' ), 
+			'email' => $this->getData ( 'email' ), 
+			'password' => $this->getData ( 'password' ), 
+			'usergroup' => Mage::helper ( 'Flagbit_Typo3connect' )->getConfigData ( 'fe_user_group_uid' ), 
+			'pid' => Mage::helper ( 'Flagbit_Typo3connect' )->getConfigData ( 'fe_user_pid' ), 
+			'tx_fbmagento_id' => $this->getId () 
 		);
 		
-		$feUsers = Mage::getSingleton('Flagbit_Typo3connect/Typo3_FeUsers');
-		$this->load($this->getId());
+		$feUsers = Mage::getSingleton ( 'Flagbit_Typo3connect/Typo3_FeUsers' );
+		$this->load ( $this->getId () );
 		
-		if($this->getTypo3_uid()){
-			$feUsers->setId($this->getTypo3_uid());
-		}		
-		
-		foreach($fields as $key => $value){
-			$feUsers->setData($key, $value);
+		if ($this->getTypo3_uid ()) {
+			$feUsers->setId ( $this->getTypo3_uid () );
 		}
-		$feUsers->save();
-
-    	$this->setData('typo3_uid', $feUsers->getId());
-    	$this->getResource()->saveAttribute($this, 'typo3_uid');
+		
+		foreach ( $fields as $key => $value ) {
+			$feUsers->setData ( $key, $value );
+		}
+		$feUsers->save ();
+		
+		$this->setData ( 'typo3_uid', $feUsers->getData ( 'uid' ) );
+		$this->getResource ()->saveAttribute ( $this, 'typo3_uid' );
 	
 	}
-	
- 
 
 }
 
--- Magento/Typo3connect/Model/Mysql4/Typo3/FeUsers.php
+++ Magento/Typo3connect/Model/Mysql4/Typo3/FeUsers.php
@@ -1,33 +1,46 @@
 <?php
 
-
 class Flagbit_Typo3connect_Model_Mysql4_Typo3_FeUsers extends Mage_Core_Model_Mysql4_Abstract {
-
-    protected function _construct()
-    {
-        $this->_init('Flagbit_Typo3connect/typo3_feusers', 'uid');
-        
-
-    }	
 	
-    /**
-     * Get an TYPO3 fe_user
-     *
-     * @param   int unique ID
-     * @return  array
-     */
-    public function getUserById($id)
-    {
-        $read = $this->_getReadAdapter();
-        $select = $read->select();
+	protected function _construct() {
+		$this->_init ( 'Flagbit_Typo3connect/typo3_feusers', 'uid' );
+		$this->_resourcePrefix = 'Flagbit_Typo3connect';
+	}
+	
+	/**
+	 * Get connection by name or type
+	 *
+	 * @param   string $connectionName
+	 * @return  Zend_Db_Adapter_Abstract
+	 */
+	protected function _getConnection($connectionName) {
+		
+		if (isset ( $this->_connections [$connectionName] )) {
+			return $this->_connections [$connectionName];
+		}
+		
+		$connConfig = Mage::getConfig ()->getResourceConnectionConfig ( 'Flagbit_Typo3connect_' . $connectionName );
+		
+		$typeInstance = $this->_resources->getConnectionTypeInstance ( ( string ) $connConfig->type );
+		$this->_connections [$connectionName] = $typeInstance->getConnection ( $connConfig );
+		
+		return $this->_connections [$connectionName];
+	}
+	
+	/**
+	 * Get an TYPO3 fe_user
+	 *
+	 * @param   int unique ID
+	 * @return  array
+	 */
+	public function getUserById($id) {
+		$read = $this->_getReadAdapter ();
+		$select = $read->select ();
+		
+		$select->from ( array ('main_table' => $this->getMainTable () ) )->where ( $this->getIdFieldName () . ' = ?', $id )->limit ( 1 );
+		
+		return $read->fetchRow ( $select );
+	}
 
-        $select->from(array('main_table'=>$this->getMainTable()))
-            ->where($this->getIdFieldName().' = ?', $id)
-            ->limit(1);
-
-        return $read->fetchRow($select);
-    }
-    
 }
 
-?>
