f5ce62c596aaa3659f1aa4610a8013854d5ae8c4
vinai <vinai@netzarbeiter.com>
2009-01-21 11:59:18 +0000

    Changed setup to use Mage_Eav_Model_Entity_Setup::addAttribute()

--- Magento/Typo3connect/sql/Flagbit_Typo3connect_setup/mysql4-install-0.1.0.php
+++ Magento/Typo3connect/sql/Flagbit_Typo3connect_setup/mysql4-install-0.1.0.php
@@ -24,26 +24,38 @@ $installer = $this;
 /* @var $installer Mage_Core_Model_Resource_Setup */
 
 $installer->startSetup();
+
+$installer->addAttribute('customer', 'typo3_uid', array(
+	'type'            => 'int',
+	'label'           => 'Typo3 UID',
+	'input'           => 'hidden',
+	'global'          => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+	'required'        => false,
+	'default'         => '',
+	'user_defined'    => 0
+));
+
+/*
 $installer->run("
 INSERT INTO {$this->getTable('eav_attribute')} (
-`attribute_id` ,
-`entity_type_id` ,
-`attribute_code` ,
-`attribute_model` ,
-`backend_model` ,
-`backend_type` ,
-`backend_table` ,
-`frontend_model` ,
-`frontend_input` ,
-`frontend_label` ,
-`frontend_class` ,
-`source_model` ,
-`is_global` ,
-`is_visible` ,
-`is_required` ,
-`is_user_defined` ,
-`default_value` ,
-`is_searchable` ,
+`attribute_id` ,    // NULL
+`entity_type_id` ,  // 1
+`attribute_code` ,  // typo3_uid
+`attribute_model` , // NULL
+`backend_model` ,   // ''
+`backend_type` ,    // int
+`backend_table` ,   // ' '
+`frontend_model` ,  // ''
+`frontend_input` ,  // hidden
+`frontend_label` ,  // ''
+`frontend_class` ,  // ''
+`source_model` ,    // ''
+`is_global` ,       // 1
+`is_visible` ,      // 1
+`is_required` ,     // 0
+`is_user_defined` , // 0
+`default_value` ,   // ''
+`is_searchable` ,   // 0
 `is_filterable` ,
 `is_comparable` ,
 `is_visible_on_front` ,
@@ -51,14 +63,15 @@ INSERT INTO {$this->getTable('eav_attribute')} (
 `is_visible_in_advanced_search` ,
 `is_configurable` ,
 `apply_to` ,
-`position` ,
-`note` ,
-`is_used_for_price_rules`
+`position` ,       // 0
+`note` ,           // ''
+`is_used_for_price_rules` // 1
 )
 VALUES (
 NULL , '1', 'typo3_uid', NULL , '', 'int', ' ', '', 'hidden', '', '', '', '1', '1', '0', '0', '', '0', '0', '0', '0', '0', '0', '0', '', '0', '', '1'
 );
 
 ");
+*/
 
 $installer->endSetup();
