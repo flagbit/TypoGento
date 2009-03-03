27744064ef8d0247427fb7a0f797ddf9a2fea1ba
drlrdsen <tuerk@flagbit.de>
2009-03-03 22:18:24 +0000

    Re-Added Store Model

---
+++ Magento/Typo3connect/Model/Store.php
@@ -0,0 +1,50 @@
+<?php
+
+class Flagbit_Typo3connect_Model_Store extends Mage_Core_Model_Store {
+	
+	/**
+	 * TYPO3 Base URL
+	 *
+	 * @var string $typo3BaseUrl
+	 */
+	protected $typo3BaseUrl;
+	
+	public function getBaseUrl($type = self::URL_TYPE_LINK, $secure = null) {
+		$cacheKey = $type . '/' . (is_null ( $secure ) ? 'null' : ($secure ? 'true' : 'false'));
+		if (! isset ( $this->_baseUrlCache [$cacheKey] )) {
+			
+			switch ($type) {
+				case self::URL_TYPE_WEB :
+					$secure = is_null ( $secure ) ? $this->isCurrentlySecure () : ( bool ) $secure;
+					$url = $this->getConfig ( 'web/' . ($secure ? 'secure' : 'unsecure') . '/base_url' );
+					break;
+				
+				case self::URL_TYPE_LINK :
+					if ($this->getTypo3BaseUrl()) {
+						$url = $this->getTypo3BaseUrl ();
+					} else {
+						$secure = ( bool ) $secure;
+						$url = $this->getConfig ( 'web/' . ($secure ? 'secure' : 'unsecure') . '/base_link_url' );
+						$url = $this->_updatePathUseRewrites ( $url );
+						$url = $this->_updatePathUseStoreView ( $url );
+					}
+					break;
+				
+				case self::URL_TYPE_SKIN :
+				case self::URL_TYPE_MEDIA :
+				case self::URL_TYPE_JS :
+					$secure = is_null ( $secure ) ? $this->isCurrentlySecure () : ( bool ) $secure;
+					$url = $this->getConfig ( 'web/' . ($secure ? 'secure' : 'unsecure') . '/base_' . $type . '_url' );
+					break;
+				
+				default :
+					throw Mage::exception ( 'Mage_Core', Mage::helper ( 'core' )->__ ( 'Invalid base url type' ) );
+			}
+			
+			$this->_baseUrlCache [$cacheKey] = rtrim ( $url, '/' ) . '/';
+		}
+		
+		return $this->_baseUrlCache [$cacheKey];
+	}
+
+}
