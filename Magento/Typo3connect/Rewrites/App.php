3a05d1e936a7c54dedfd990afa6e6449d8f4f207
hackwell <weller@flagbit.de>
2009-10-08 09:08:13 +0000

    added APP Rewrite for Response Model Replacement

---
+++ Magento/Typo3connect/Rewrites/App.php
@@ -0,0 +1,18 @@
+<?php
+
+class Mage_Core_Model_App extends Flagbit_Typo3connect_Rewrite_Core_Model_App
+{
+	
+
+    /**
+     * Retrieve response object
+     *
+     * @return Zend_Controller_Response_Http
+     */
+    public function getResponse()
+    {
+        return Mage::getSingleton('Flagbit_Typo3connect/Core')->getResponse();
+    }	
+	
+	
+}
