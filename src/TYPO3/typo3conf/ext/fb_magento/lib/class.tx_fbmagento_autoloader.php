e8ae6adaa664de8fc4952056b4755144bba13567
Frederic Gaus, Flagbit GmbH & Co. KG <gaus@flagbit.de>
2011-09-09 14:19:48 +0200

    [CLEANUP] Typo in comment and code cleanups

--- src/TYPO3/typo3conf/ext/fb_magento/lib/class.tx_fbmagento_autoloader.php
+++ src/TYPO3/typo3conf/ext/fb_magento/lib/class.tx_fbmagento_autoloader.php
@@ -19,35 +19,33 @@
  * @author Frederic Gaus <gaus@flagbit.de>
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License, version 2
  */
-
-
-
 class tx_fbmagento_autoloader implements t3lib_Singleton {
-	
+
 	/**
 	 * constructor for tx_fbmagento_interface
 	 *
 	 * @param array emConf
 	 */
 	public function __construct($config) {
-		
+
 		// init Config Array
 		$this->config = $config;
-		
+
 		// include Mage
 		if(!class_exists('Mage', false)){
 			require_once ($this->config ['path'] . 'app/Mage.php');
 		}
-		
+
 		// disable Notices
 		error_reporting ( E_ALL & ~ E_NOTICE );		
-		
+
 		// overwrite Magento Autoload Funktion
 		if(class_exists('Varien_Autoload', false)){
 			spl_autoload_unregister(array(Varien_Autoload::instance(), 'autoload'));
 		}
-		spl_autoload_register ( array (&$this, 'autoload' ) );		
+		spl_autoload_register ( array (&$this, 'autoload' ) );
 	}
+
 
 	/**
 	 * generate a duplicate of a Class with an other Name
@@ -57,16 +55,16 @@ class tx_fbmagento_autoloader implements t3lib_Singleton {
 	 * @return string
 	 */
 	protected function rewriteClass($className){
-	
+
 		// cache Path
 		$cachePath = $this->config['path'].'var/cache/';
-		
+
 		// get Filename from Classname
 		$fileName = $this->_getClassPath($className);
-		
+
 		// generate a new Version of Classfile if not exists
 		if(!file_exists($cachePath.$fileName)){
-			
+
 			// get source of the original Class
 			$content = file_get_contents($this->config['path'].'app/code/core/'.uc_words ( $className, DS ) . '.php');
 
@@ -75,14 +73,15 @@ class tx_fbmagento_autoloader implements t3lib_Singleton {
 
 			// write new Class
 			$classPath = substr($fileName, 0, strrpos($fileName, '/') + 1);
-			
+
 			t3lib_div::mkdir_deep($cachePath, substr($fileName, 0, strrpos($fileName, '/') + 1));
 			t3lib_div::writeFile($cachePath.$fileName, $content);
 		}
-		
+
 		return $cachePath.$fileName;
 	}
-	
+
+
 	/**
 	 * get absolute class path by class name
 	 * 
@@ -90,9 +89,9 @@ class tx_fbmagento_autoloader implements t3lib_Singleton {
 	 * @retirn string
 	 */
 	protected function _getClassPath($className){
-		
+
 		$fileName = $this->_getFilename($className);
-		
+
 		if(substr($fileName,1,1) != '/'){
 			$includePaths = explode(':',get_include_path());
 			foreach($includePaths as $includePath){
@@ -105,7 +104,8 @@ class tx_fbmagento_autoloader implements t3lib_Singleton {
 			}
 		}
 	}
-	
+
+
 	/**
 	 * get the Filename of a Class
 	 *
@@ -116,7 +116,8 @@ class tx_fbmagento_autoloader implements t3lib_Singleton {
 		$filename = uc_words ( $className, DS) . '.php';
 		return $filename;
 	}
-	
+
+
 	/**
 	 * Class autoload
 	 *
@@ -124,33 +125,33 @@ class tx_fbmagento_autoloader implements t3lib_Singleton {
 	 * @param string $class
 	 */
 	public function autoload($class) {
-		
+
 		if (strpos ( $class, DS ) !== false) {
 			return;
 		}
-		
+
 		// to some dirty Class reflection because of Mages unrewriteable Classes
 		$filename = $this->_getFilename($class);
 		$rewritePath = $this->config['path'].'app/code/'.$this->config['namespace'].'/Flagbit/Typo3connect/Rewrites/'.$filename;
-		
+
 		if(file_exists($rewritePath) 
 			&& $filename != '.php' && $filename){
-			
+
 			$newClassFile = $this->rewriteClass($class);
 			include($newClassFile);
-						
+
 			include($rewritePath);
 			return;
-		}		
-		
+		}
+
 		$classFile = uc_words ( $class, DS ) . '.php';
-		
+
 		try{
 			@include ($classFile);
 		}catch (Exception $m){
-			// no output since TYPO3 Classes will also be loaded throw autoload
+			// no output since TYPO3 Classes will also be loaded through autoload
 		}
-	}	
+	}
 }
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/fb_magento/lib/class.tx_fbmagento_autoloader.php']) {
